0e7e7e9750c74a85fa4478a4d13a2c26221a3e65
Светка <sveta.chakrygina@mail.ru>
2022-09-07 20:38:52 +0300

    Start

---
+++ src/Employee.java
@@ -0,0 +1,18 @@
+public class Employee {
+    private String fullName;
+    private int numDepartment;
+    private float salary;
+    private static int counter;
+    private int id;
+
+public Employee(String fullName, int numDepartment, float salary, int counter, int id) {
+    this.fullName = fullName;
+    this.numDepartment = numDepartment;
+    this.salary = salary;
+    this.id = id;
+
+}
+
+
+
+}
