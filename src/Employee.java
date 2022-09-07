54e84b7f60993df5fc40eefbcff74b324786b9dc
Светка <sveta.chakrygina@mail.ru>
2022-09-07 20:53:55 +0300

    Get and set

--- src/Employee.java
+++ src/Employee.java
@@ -5,14 +5,39 @@ public class Employee {
     private static int counter;
     private int id;
 
-public Employee(String fullName, int numDepartment, float salary, int counter, int id) {
-    this.fullName = fullName;
-    this.numDepartment = numDepartment;
-    this.salary = salary;
-    this.id = id;
+    public Employee(String fullName, int numDepartment, float salary, int counter, int id) {
+        this.fullName = fullName;
+        this.numDepartment = numDepartment;
+        this.salary = salary;
+        this.id = id;
+    }
 
+    public String getFullName() {
+        return fullName;
+    }
+
+    public int getNumDepartment() {
+        return numDepartment;
+    }
+
+    public float getSalary() {
+        return salary;
+    }
+
+    public static int getCounter() {
+        return counter;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setNumDepartment(int numDepartment) {
+        this.numDepartment = numDepartment;
+    }
+
+    public void setSalary(float salary) {
+        this.salary = salary;
+    }
 }
 
-
-
-}
