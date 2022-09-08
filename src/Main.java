549c622d914a06a4e9d3de81e1ee942de888b7ee
Светка <sveta.chakrygina@mail.ru>
2022-09-08 14:16:11 +0300

    Get, set, constructor, main

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,18 @@
 public class Main {
 
     public static void main(String[] args) {
+
         Employee[] employees = new Employee[10];
-        employees[0] = new Employee("Булгаков Михаил Афанасьевич", 3, 57_780.73f,1);
-        employees[1] = new Employee("Чехов Антон Павлович", 1, 45_530.21f,2);
-        employees[2] = new Employee("Ахматова Анна Андреевна", 2, 53_420.46f,3);
-        employees[3] = new Employee("Маяковский Владимир Владимирович", 4, 60_220.15f,4);
-        employees[4] = new Employee("Есенин Сергей Александрович", 3, 56_670.89f,5);
-        employees[5] = new Employee("Лаппа Татьяна Николаевна", 5, 62_490.34f,6);
-        employees[6] = new Employee("Пастернак Борис Леонидович", 4, 59_930.65f,7);
-        employees[7] = new Employee("Блок Александр Александрович", 2, 53_340.37f,8);
-        employees[8] = new Employee("Цветаева Марина Ивановна", 1, 44_900.12f,9);
-        employees[9] = new Employee("Полонская Вероника Витольдовна", 5, 63_600.10f,10);
+        employees[0] = new Employee("Булгаков Михаил Афанасьевич", 3, 57_780.73f, 1);
+        employees[1] = new Employee("Чехов Антон Павлович", 1, 45_530.21f, 2);
+        employees[2] = new Employee("Ахматова Анна Андреевна", 2, 53_420.46f, 3);
+        employees[3] = new Employee("Маяковский Владимир Владимирович", 4, 60_220.15f, 4);
+        employees[4] = new Employee("Есенин Сергей Александрович", 3, 56_670.89f, 5);
+        employees[5] = new Employee("Лаппа Татьяна Николаевна", 5, 62_490.34f, 6);
+        employees[6] = new Employee("Пастернак Борис Леонидович", 4, 59_930.65f, 7);
+        employees[7] = new Employee("Блок Александр Александрович", 2, 53_340.37f, 8);
+        employees[8] = new Employee("Цветаева Марина Ивановна", 1, 44_900.12f, 9);
+        employees[9] = new Employee("Полонская Вероника Витольдовна", 5, 63_600.10f, 10);
 
         System.out.println(employees[0]);
         System.out.println(employees[1]);
@@ -24,15 +25,7 @@ public class Main {
         System.out.println(employees[8]);
         System.out.println(employees[9]);
 
-
     }
-
-
-
-
-
-
-
-
-
 }
+
+
